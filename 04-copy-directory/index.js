478885117b01875c876b5852981264e7f2d78b1c
AntonovichVladimir <antonovichvladimira@gmail.com>
2022-06-05 13:03:54 +0300

    fix: change solution in 04 and 05 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,50 +10,40 @@ fs.promises
     console.log("failed to create directory");
   });
 
-let copyFiles = () => {
-  fs.readdir(
-    path.join(__dirname, "files"),
-    { withFileTypes: true },
-    (error, files) => {
-      if (error) {
-        console.log(error);
-      } else {
-        files.forEach((file) => {
-          if (file.isFile()) {
-            fs.promises.copyFile(
-              path.join(__dirname, "files", file.name),
-              path.join(__dirname, "files-copy", file.name)
-            );
-          }
-        });
-      }
-    }
-  );
-};
-
 fs.readdir(
   path.join(__dirname, "files-copy"),
   { withFileTypes: true },
   (error, files) => {
-    if (error) return console.log(error);
-
-    const removeFiles = [];
-
-    files.forEach((file) => {
-      removeFiles.push(
-        new Promise((resolve, reject) => {
+    if (error) console.log(error);
+    else {
+      files.forEach((file) => {
+        if (file.isFile()) {
           fs.unlink(path.join(__dirname, "files-copy", file.name), (error) => {
             if (error) {
-              reject(error);
               console.log(error);
-            } else {
-              resolve();
             }
           });
-        })
-      );
-    });
+        }
+      });
+    }
+  }
+);
 
-    Promise.all(removeFiles).then(copyFiles);
+fs.readdir(
+  path.join(__dirname, "files"),
+  { withFileTypes: true },
+  (error, files) => {
+    if (error) {
+      console.log(error);
+    } else {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          fs.promises.copyFile(
+            path.join(__dirname, "files", file.name),
+            path.join(__dirname, "files-copy", file.name)
+          );
+        }
+      });
+    }
   }
 );
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,44 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readdir(
-  path.join(__dirname, "styles"),
-  { withFileTypes: true },
-  (error, files) => {
-    if (error) console.log(error);
-    else {
-      console.log(
-        "bundle.css file successfully created\nStyles successfully added to the bundle.css file"
-      );
-      let read = [];
-      let bundleStream = fs.createWriteStream(
-        path.join(__dirname, "project-dist", "bundle.css"),
+const bundleStream = fs.createWriteStream(
+  path.join(__dirname, "project-dist", "bundle.css")
+);
+
+fs.readdir(path.join(__dirname, "styles"), (error, data) => {
+  if (error) console.log(error);
+  for (let i = 0; i < data.length; i++) {
+    let extension = path.extname(data[i]).split(".").pop();
+    if (extension === "css") {
+      let stylesStream = fs.createReadStream(
+        path.join(__dirname, "styles", data[i]),
         "utf-8"
       );
-
-      files.forEach((file) => {
-        if (
-          path.extname(path.join(__dirname, "styles", file.name)) === ".css"
-        ) {
-          read.push(
-            new Promise((resolve, reject) => {
-              const stylesStream = fs.createReadStream(
-                path.join(__dirname, "styles", file.name),
-                "utf8"
-              );
-
-              stylesStream.on("data", (chunk) => {
-                bundleStream.write(chunk);
-              });
-
-              stylesStream.on("end", () => resolve());
-              stylesStream.on("error", (e) => reject(e));
-            })
-          );
-        }
+      stylesStream.on("data", (chunk) => {
+        bundleStream.write(chunk);
       });
-
-      read.reduce((previous, current) => previous.then(() => current));
     }
   }
-);
+});
